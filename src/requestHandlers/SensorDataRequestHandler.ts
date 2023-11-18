396dfe7d60e47f171c98aa5a4b21118cc71f44d6
Juan Diego Barrios <juan.barrios@qrador.com>
2023-11-17 19:07:09 -0500

    feat: add connection to sitewise

--- src/requestHandlers/SensorDataRequestHandler.ts
+++ src/requestHandlers/SensorDataRequestHandler.ts
@@ -1,8 +1,37 @@
 import { Context } from 'koa';
+import { IoTSiteWiseClient, BatchPutAssetPropertyValueCommand, BatchPutAssetPropertyValueRequest } from "@aws-sdk/client-iotsitewise";
 
 class SensorDataRequestHandler {
   public static async postSensorData(ctx: Context): Promise<void> {
-    console.log('post sensor data')
+    const client = new IoTSiteWiseClient();
+    const timeInSeconds = Math.round(new Date().getTime() / 1000);
+    const entryId = timeInSeconds.toString();
+    const propertyAlias = '/AquaponicSystem/FishTankTemperatureSensor/temperature';
+    const temperature = 17.2;
+    const quality = 'GOOD';
+
+    const input: BatchPutAssetPropertyValueRequest = {
+      entries: [
+        { 
+          entryId,
+          propertyAlias,
+          propertyValues: [
+            { 
+              value: { 
+                doubleValue: temperature,
+              },
+              timestamp: {
+                timeInSeconds,
+              },
+              quality,
+            },
+          ],
+        },
+      ],
+    };
+    const command = new BatchPutAssetPropertyValueCommand(input);
+    // TODO check errorEntries
+    const response = await client.send(command);
 
     ctx.body = 'post sensor data';
     ctx.status = 200;
