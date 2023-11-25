ae75cc65713cb8eb33f4c9539a66d3d43a108c36
Juan Diego Barrios <juan.barrios@qrador.com>
2023-11-25 00:08:18 -0500

    feat: add switch case to simulateMQTTTPublications script

--- src/scripts/simulateMQTTPublications/script.ts
+++ src/scripts/simulateMQTTPublications/script.ts
@@ -1,21 +1,48 @@
 import { connect } from 'mqtt';
 import simulateNumericIncreaseDecreaseRandomMQTTPublications from './mqttPublicationSimulators/simulateNumericIncreaseDecreaseRandomMQTTPublications';
-import { fishTankWaterLevelSensorHasNeededWaterLevelMQTTPublicationsSimulationProps, fishTankWaterTemperatureSensorTemperatureMQTTPublicationsSimulationProps } from '../../mocks/mqttPublicationsSimulationsProps';
+import mqttPublicationsSimulationsProps, { fishTankWaterTemperatureSensorTemperatureMQTTPublicationsSimulationProps } from '../../mocks/mqttPublicationsSimulationsProps';
+import simulateNumericIncreaseMQTTPublications from './mqttPublicationSimulators/simulateNumericIncreaseMQTTPublications';
+import simulateNumericDecreaseMQTTPublications from './mqttPublicationSimulators/simulateNumericDecreaseMQTTPublications';
+import simulateNumericRandomMQTTPublications from './mqttPublicationSimulators/simulateNumericRandomMQTTPublications';
+import MQTTPublicationsSimulationProps, { SimulationType } from '../../interfaces/MQTTPublicationsSimulationProps';
+import simulateStaticMQTTPublications from './mqttPublicationSimulators/simulateStaticMQTTPublications';
 import simulateArrayOfPosibleValuesRandomMQTTPublications from './mqttPublicationSimulators/simulateArrayOfPosibleValuesRandomMQTTPublications';
 
 async function script(): Promise<void> {
   console.log('-- Mock sensors data');
   const mqttClient = connect("mqtt://test.mosquitto.org");
-  // measurements.forEach((measurement: Measurement) => {
-  //   const { aliasPath, simulationProps } = measurement;
+  mqttPublicationsSimulationsProps.forEach((mqttPublicationsSimulationProps: MQTTPublicationsSimulationProps) => {
+    const { simulationType } = mqttPublicationsSimulationProps;
 
-  //   if (simulationProps) {
+    switch (simulationType) {
+      case SimulationType.STATIC:
+        simulateStaticMQTTPublications(mqttClient, mqttPublicationsSimulationProps);
+        break;
 
-  //   }
-  // });
+      case SimulationType.NUMERIC_RANDOM:
+        simulateNumericRandomMQTTPublications(mqttClient, mqttPublicationsSimulationProps);
+        break;
 
-  const MQTTPublicationsSimulationProps = fishTankWaterLevelSensorHasNeededWaterLevelMQTTPublicationsSimulationProps;
-  simulateArrayOfPosibleValuesRandomMQTTPublications(mqttClient, MQTTPublicationsSimulationProps);
+      case SimulationType.NUMERIC_INCREASE:
+        simulateNumericIncreaseMQTTPublications(mqttClient, mqttPublicationsSimulationProps);
+        break;
+
+      case SimulationType.NUMERIC_DECREASE:
+        simulateNumericDecreaseMQTTPublications(mqttClient, mqttPublicationsSimulationProps);
+        break;
+
+      case SimulationType.NUMERIC_INCREASE_DECREASE_RANDOM:
+        simulateNumericIncreaseDecreaseRandomMQTTPublications(mqttClient, mqttPublicationsSimulationProps);
+        break;
+
+      case SimulationType.ARRAY_OF_POSIBLE_VALUES_RANDOM:
+        simulateArrayOfPosibleValuesRandomMQTTPublications(mqttClient, mqttPublicationsSimulationProps);
+        break;
+
+      default:
+        break;
+    }
+  });
 }    
 
 
